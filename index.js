d2ee9c6d64398961ee01e8475aa98f090f67375f
naimulhassan2001 <developernaimul00@gmail.com>
2024-05-01 00:52:44 +0600

    improve code

--- index.js
+++ index.js
@@ -3,20 +3,15 @@ const app = express();
 
 app.use(express.json());
 
-app.locals.serverError = { Status: false, Message: "Internal server error" };
-app.locals.clientError = { Status: false, Message: "Client side error" };
+const logger = (req, res, next) => {
+  console.log(req.url);
+  next();
+};
 
-app
-  .route("/admin/auth")
-  .get((req, res) => {
-    res.send("this is home page with get request");
-  })
-  .post((req, res) => {
-    res.send("this is home page with post request");
-  });
-
+app.use(logger);
 app.get("/user/:id", (req, res) => {
-  res.send(app.locals.clientError);
+  console.log(req.params.id)
+  res.send("this is home page with get request");
 });
 
 app.post("/", (req, res) => {
