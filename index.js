0a1a180d7ab66580bdb4cfe1ee68105244cf8c45
naimulhassan2001 <developernaimul00@gmail.com>
2024-05-01 13:01:28 +0600

    middleware

--- index.js
+++ index.js
@@ -4,13 +4,23 @@ const app = express();
 app.use(express.json());
 
 const logger = (req, res, next) => {
-  console.log(req.url);
-  next();
+  console.log("Request Body:", req.body);
+  console.log("URL:", `${req.protocol}://${req.hostname}${req.originalUrl}`);
+
+  next("null");
+};
+
+const errorHandler = (err, req, res, next) => {
+  res.status(500).json({
+    Status: false,
+    Message: "internal server error",
+  });
 };
 
 app.use(logger);
+app.use(errorHandler);
 app.get("/user/:id", (req, res) => {
-  console.log(req.params.id)
+  console.log(req.params.id);
   res.send("this is home page with get request");
 });
 
@@ -18,6 +28,7 @@ app.post("/", (req, res) => {
   console.log(req.body);
   res.send("this is home page with post request");
 });
+
 
 app.listen(3000, () => {
   console.log("server listening in 3000 port");
